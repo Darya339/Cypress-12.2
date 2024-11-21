7462d420062cd9951bdc619746a18a2a9071924d
Darya339 <gribencenkodara@gmail.com>
2024-11-21 10:00:03 +0300

    Add files via upload

---
+++ new_cypress-main/cypress/e2e/skelet.cy.js
@@ -0,0 +1,7 @@
+ describe('Покупка аватара', function () {                                
+    it('e2e тест на покупку нового аватара для тренера', function () {   
+         cy.visit('https://pokemonbattle.ru/'); 
+          
+         
+        })
+    })
